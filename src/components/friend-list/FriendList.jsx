2a28e5f8eb2184ce22495f25a58c2a64af2e2e79
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-04-16 13:15:19 +0600

    made the friend list responsive

--- src/components/friend-list/FriendList.jsx
+++ src/components/friend-list/FriendList.jsx
@@ -8,7 +8,7 @@ const FriendList = () => {
     return (
         <div className=" max-w-277.5 mx-auto pb-4">
             <h1 className="pt-10 mb-4 font-semibold text-2xl text-left">Your Friends</h1>
-            <div className="grid grid-cols-4 gap-6">
+            <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 {friends.map((friend) => (
                     <Friend key={friend.id} friend={friend}></Friend>
                 ))}
